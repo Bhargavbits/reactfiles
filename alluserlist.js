50d2092a606e1eb47dc5e71813a92b1d2d540e4c
bhargav rana <118766633+Bhargavbits@users.noreply.github.com>
2023-01-27 21:37:55 +0530

    Create alluserlist.js

---
+++ alluserlist.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const AllUserList = () => {
+    I
+    let allUsers = [
+        {
+            name: "John",
+            email: "john@gmail.com",
+            phone: "+91-9876543210"
+        },
+        { name: "Jonny", email: "jonnyxyz@gmail.com", phone: "+91-9876543210" },
+        { name: "Alex", email: "alexxyz@gmail.com", phone: "+91-9878343210" },
+    ];
+};
+
+let array = [
+    <userDetails user={allUsers{0}} />,
+        < userDetails user = { allUsers{ 1 }} />,
+            < userDetails user = { allUsers{ 2 }} />,
+];
+return <div>{allUsers[0]}</div>;
+export default AllUserList;
